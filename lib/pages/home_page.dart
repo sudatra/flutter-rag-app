638764819749f2704be017c93854c33818e63553
sudatra <mail2sudatra@gmail.com>
2025-06-21 13:46:50 +0530

    added search section and footer, completed home-page ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rag/widgets/search_section.dart';
 import 'package:flutter_rag/widgets/side_bar.dart';
+import 'package:flutter_rag/theme/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,7 +11,89 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Row(
         children: [
-          SideBar()
+          SideBar(),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: SearchSection()
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pro",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Blob",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Enterprise",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Store",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Careers",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "English (English)",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       )
     );
---
+++ lib/widgets/search_bar_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rag/theme/colors.dart';
+
+class SearchBarButton extends StatefulWidget {
+  final IconData icon;
+  final String text;
+
+  const SearchBarButton({
+    super.key,
+    required this.icon,
+    required this.text
+  });
+
+  @override
+  State<SearchBarButton> createState() => _SearchBarButtonState();
+}
+
+class _SearchBarButtonState extends State<SearchBarButton> {
+  bool isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          isHover = true;
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          isHover = false;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: isHover ? AppColors.proButton : Colors.transparent
+        ),
+        child: Row(
+          children: [
+            Icon(
+              widget.icon,
+              color: AppColors.iconGrey,
+              size: 20,
+            ),
+            
+            const SizedBox(width: 8),
+            
+            Text(
+              widget.text,
+              style: TextStyle(
+                color: AppColors.textGrey
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
