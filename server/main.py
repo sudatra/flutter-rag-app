3459203d6e45b789ef5138ed1814a858a2c4eb41
sudatra <mail2sudatra@gmail.com>
2025-06-22 17:27:09 +0530

    added basic chat endpoint setup and tavily setup

--- server/main.py
+++ server/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from pydantic_models.chat_body import ChatBody
+from services.search_service import SearchService
 
 app = FastAPI()
+search_service = SearchService()
+
+# CHAT API
+@app.post("/chat")
+def chat_endpoint(body: ChatBody):
+  search_service.web_search(body.query)
+  return body.query
