8fa5563f62616f3b1d5f61040859f4cadfd8c8ff
sudatra <mail2sudatra@gmail.com>
2025-06-23 20:12:20 +0530

    added cosine similarity and embedding generation

--- server/main.py
+++ server/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from pydantic_models.chat_body import ChatBody
+from services.sort_source_service import SortSourceService
 from services.search_service import SearchService
 
 app = FastAPI()
 search_service = SearchService()
+sort_source_service = SortSourceService()
 
 # CHAT API
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
   search_results = search_service.web_search(body.query)
-  print(search_results)
+  sorted_results = sort_source_service.sort_sources(body.query, search_results)
+
+  print(sorted_results)
+
   return body.query
--- server/services/sort_source_service.py
+++ server/services/sort_source_service.py
@@ -1,5 +1,31 @@
 from typing import List
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
 class SortSourceService:
+  def __init__(self):
+    self.embedding_model = SentenceTransformer("all-miniLM-L6-v2")
+  
   def sort_sources(self, query: str, search_results: List[dict]):
-    pass
+    relevant_docs = []
+    query_embedding = self.embedding_model.encode(query)
+    
+    for result in search_results:
+      try:
+        content = result.get('content')
+        if not content:
+          continue
+
+        res_embedding = self.embedding_model.encode(content)
+        print(res_embedding)
+        similarity = (np.dot(query_embedding, res_embedding) / (np.linalg.norm(query_embedding) * np.linalg.norm(res_embedding)))
+        print(similarity)
+        result['relevance_score'] = similarity
+
+        if similarity > 0.3:
+          relevant_docs.append(result)
+      except Exception as e:
+        print(f"Skipping result due to error...")
+        continue
+
+    return sorted(relevant_docs, key=lambda x: x['relevance_score'], reverse=True)
